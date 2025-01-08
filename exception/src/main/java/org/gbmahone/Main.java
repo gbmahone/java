d8a33f58b87417e60d637982f9e235073fe80781
gbmahone <oliveiragabrielam@hotmail.com>
2025-01-08 11:13:34 +0000

    balance account exception

---
+++ exception/src/main/java/org/gbmahone/Main.java
@@ -0,0 +1,43 @@
+package org.gbmahone;
+
+import org.gbmahone.entities.Account;
+import org.gbmahone.entities.BusinessException;
+
+import java.lang.reflect.AccessFlag;
+import java.util.Locale;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data ");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Initial balance: ");
+        double balance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account acc = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
+        System.out.print("Enter amount for withdraw: ");
+        double amount = sc.nextDouble();
+
+        try {
+            acc.withdraw(amount);
+            System.out.println("New balance: " + String.format("%.2f", acc.getBalance()));
+        }
+        catch (BusinessException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
