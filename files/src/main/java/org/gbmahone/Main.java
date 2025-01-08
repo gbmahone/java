53424adba1e7c12a4efb9cdc5380806ca0460a2f
gbmahone <oliveiragabrielam@hotmail.com>
2025-01-08 12:15:56 +0000

    WIP - create files with FW and BW

--- files/src/main/java/org/gbmahone/Main.java
+++ files/src/main/java/org/gbmahone/Main.java
@@ -1,28 +1,24 @@
 package org.gbmahone;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 
 public class Main {
     public static void main(String[] args) {
+        String[] lines = new String[]{"Good morning", "Good afternoon", "Good night"};
 
-     String path = "c:\\workspace\\in.txt";
+        String path = "c:\\workspace\\out.txt";
 
-     try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-         String line = br.readLine();
-        // esse readline vai ler uma linha do arquivo e se estiver no final do arquivo
-         // vai me retornar nullo
-
-         while (line != null) {
-             System.out.println(line);
-             line = br.readLine();
-         }
-     }
-     catch (IOException e) {
-         System.out.println("Error: " + e.getMessage());
-     }
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine(); //bw.newLine é pra dar a quebra de linha
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
 }
