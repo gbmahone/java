8076f6c7f1daf09cf507ddbf2f3c3a5116f963ff
gbmahone <oliveiragabrielam@hotmail.com>
2025-01-09 11:58:34 +0000

    folder manipulation File

--- files/src/main/java/org/gbmahone/Main.java
+++ files/src/main/java/org/gbmahone/Main.java
@@ -1,37 +1,66 @@
 package org.gbmahone;
 
+import org.gbmahone.entities.Product;
+
 import java.io.*;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a folder path: ");
-        String strPath = sc.nextLine();
+        List<Product> list = new ArrayList<>();
 
-        File path = new File(strPath);
+        System.out.println("Enter file path: ");
+        String sourceFileStr = sc.nextLine();
 
-        // vetor das pastas com uma referencia que especifica somente quem é pasta(File::isDirectory)
-        File[] folders = path.listFiles(File::isDirectory);
-        System.out.println("FOLDERS:");
-        for (File folder : folders) {
-            System.out.println(folder);
+        File sourceFile = new File(sourceFileStr);
+        String sourceFolderStr = sourceFile.getParent();
+
+        System.out.println(sourceFolderStr);
+
+        // mkdir é pra criar uma nova pasta(diretorio)
+        boolean success = new File(sourceFolderStr + "/out").mkdir();
+
+        String targetFileStr = sourceFolderStr + "/out/summary.csv ";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+
+            String itemCsv = br.readLine();
+            while (itemCsv != null){
+                String[] fields = itemCsv.split(", ");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Product(name, price, quantity));
+
+                itemCsv = br.readLine();
+            }
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+
+                for (Product item : list){
+                    bw.write(item.getName() + ", " + String.format("%.2f",item.total()));
+                    bw.newLine();
+                }
+
+                System.out.println(targetFileStr + "CREATED");
+
+            }catch (IOException e) {
+                System.out.println("Error writing file: " + e.getMessage());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error writing file: " + e.getMessage());
         }
-        // vetor com a lista somente de arquivos(File::isFile)
-        File[] files = path.listFiles(File::isFile);
-        System.out.println("FILES:");
-        for (File file : files){
-            System.out.println(file);
-        }
 
-        // criar uma sub pasta chamada subdir a partir da pasta informada no strPath
-        //o uso do boolean é pra confirmar se a ação foi feita
-        //mkdir() é pra criar a pasta
-        boolean success = new File(strPath + "\\subdir").mkdir();
-        System.out.println("Directory created successfully: " + success);
     }
 
 }
