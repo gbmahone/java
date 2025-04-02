ef9136c22d23a98ea5662e7df700d4f887930d73
gbmahone <oliveiragabrielam@hotmail.com>
2025-04-02 15:46:15 +0100

    heritage classes

---
+++ heritage/src/main/java/org/gbmahone/entities/BusinessAccount.java
@@ -0,0 +1,30 @@
+package org.gbmahone.entities;
+
+public class BusinessAccount extends Account{
+    // herança: extends Account vai falar pro codigo que tudo que está na classe Account tbm serve nessa classe
+
+    private Double loanLimit;
+
+    public BusinessAccount(){
+       super();
+    }
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount){
+        if (amount <= loanLimit){
+            balance += amount - 10.0;
+        }
+    }
+}
