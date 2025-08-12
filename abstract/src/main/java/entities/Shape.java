b2b74771a9a16084c4c09e6a584c591ad9b742d5
gbmahone <oliveiragabrielam@hotmail.com>
2025-08-12 17:29:16 +0100

    Interface + Heritage

--- abstract/src/main/java/entities/Shape.java
+++ abstract/src/main/java/entities/Shape.java
@@ -1,21 +1,6 @@
 package entities;
 
-public abstract class Shape {
-    private Color color;
+public interface Shape {
 
-    public Shape() {
-    }
-    public Shape(Color color) {
-        this.color = color;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    public abstract double area();
+    double area();
 }
