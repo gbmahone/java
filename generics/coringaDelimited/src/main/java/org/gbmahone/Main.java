d3e2a5da60fdeaf4d515b5300167ac315f7a7a55
gbmahone <oliveiragabrielam@hotmail.com>
2025-08-20 12:44:24 +0100

    coringa delimited

---
+++ generics/coringaDelimited/src/main/java/org/gbmahone/Main.java
@@ -0,0 +1,34 @@
+package org.gbmahone;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+        List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+        List<Object> myObjs = new ArrayList<Object>();
+
+        copy(myInts, myObjs);
+        printList(myObjs);
+        copy(myDoubles, myObjs);
+        printList(myObjs);
+    }
+    public static void copy(List<? extends Number> source, List<? super Number> destiny){
+        for (Number number : source) {
+            destiny.add(number);
+        }
+
+    }
+    public static void printList(List<?> list) {
+        for (Object item : list) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+}
