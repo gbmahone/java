cc2ec738d8be8f2fa60d6a457ec1fd7aaa478394
gbmahone <oliveiragabrielam@hotmail.com>
2025-09-01 16:33:24 +0100

    set - treeset

--- generics/set/src/main/java/org/gbmahone/main.java
+++ generics/set/src/main/java/org/gbmahone/main.java
@@ -1,22 +1,54 @@
 package application;
+import entities.Product;
+
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 public class main {
     public static void main(String[] args) {
-        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
+
+        Set<Product> set = new HashSet<>();
+
+        set.add(new Product("TV", 900.00));
+        set.add(new Product("Notebook", 1200.00));
+        set.add(new Product("Tablet", 400.00));
+
+        for(Product p : set){
+            System.out.println(p);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
         Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
-//union
+
+        //union
         Set<Integer> c = new TreeSet<>(a);
         c.addAll(b);
         System.out.println(c);
-//intersection
+
+        //intersection
         Set<Integer> d = new TreeSet<>(a);
         d.retainAll(b);
         System.out.println(d);
-//difference
+
+        //difference
         Set<Integer> e = new TreeSet<>(a);
         e.removeAll(b);
         System.out.println(e);
+        */
+
     }
 }
