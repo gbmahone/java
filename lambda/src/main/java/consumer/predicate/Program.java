727767ced3682547320960dec65e90f4d8f8b1c8
gbmahone <oliveiragabrielam@hotmail.com>
2025-11-03 17:22:18 +0000

    função

--- lambda/src/main/java/consumer/predicate/Program.java
+++ lambda/src/main/java/consumer/predicate/Program.java
@@ -1,11 +1,10 @@
-package predicate;
+package consumer.predicate;
 
-import consumer.predicate.util.PriceUpdate;
 import consumer.predicate.entities.Product;
-
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Program {
     public static void main(String[] args){
@@ -16,7 +15,9 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::staticPriceUpdate);
+        Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
+
+        list.forEach(cons);
         list.forEach(System.out::println);
     }
 }
